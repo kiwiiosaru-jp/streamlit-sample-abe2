c9a3c308486130298d2811ee4def5a6d855148ed
kiwiiosaru-jp <84110083+kiwiiosaru-jp@users.noreply.github.com>
2021-05-14 20:29:04 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -69,7 +69,7 @@ if upload_file is not None:
 			fontsize = 9       
 		fnt = ImageFont.truetype(ttfontname, fontsize)
 		draw = ImageDraw.Draw(img)
-		draw.text((rect['left'],rect['top']-fontsize*7.3),text, font=fnt, fill='yellow')
+		draw.text((rect['left'],rect['top']-fontsize*7.3),text, font=fnt, fill='blue')
 		draw.rectangle([(rect['left'],rect['top']),(rect['left']+rect['width'],rect['top']+rect['height'])],fill=None,outline='green',width=3)
 
 	st.image(img,caption='Uploaded Image', use_column_width=True)
