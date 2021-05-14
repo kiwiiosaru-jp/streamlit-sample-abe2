7ec1437d3ab91fa17e4766bf3e6f87a70c974d84
kiwiiosaru-jp <84110083+kiwiiosaru-jp@users.noreply.github.com>
2021-05-14 11:56:40 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -39,11 +39,6 @@ if upload_file is not None:
 	}
 	res = requests.post(face_api_url, params=params,headers=headers, data=binary_img)
 	results = res.json()
-	#fnt = ImageFont.truetype("./font/HannariMincho-Regular.otf", 15)
-	ttfontname = "./font/HannariMincho-Regular.otf"
-	fontsize = int(rect['width']/6)
-	fnt = ImageFont.truetype(ttfontname, fontsize)
-
 	for result in results:
 	    rect = result['faceRectangle']
 	    attr = result['faceAttributes']
@@ -59,14 +54,20 @@ if upload_file is not None:
 	    fear  = int(attr['emotion']['fear']*100)
 	    neutral  = int(attr['emotion']['neutral']*100)   
 	    if attr['makeup']['eyeMakeup']:
-	        eyeMakeup= "化粧"
+	        eyeMakeup = "化粧"
 	    else:
-	        eyeMakeup= "スッピン"
+	        eyeMakeup = "スッピン"
 	    if attr['makeup']['lipMakeup']:
-	        lipMakeup= "化粧"
+	        lipMakeup = "化粧"
 	    else:
-	        lipMakeup= "スッピン"  
+	        lipMakeup = "スッピン"  
 	    text = "[性]"+str(gender)+" "+"[齢]"+str(age)+"才"+"\n"+"[怒]"+str(anger)+"%"+" "+"[楽]"+str(happiness)+"%"+"\n"+"[悲]"+str(sadness)+"%"+" "+"[驚]"+str(surprise)+"%"+"\n"+"[恐]"+str(fear)+"%"+" "+"[平]"+str(neutral)+"%"+"\n"+"[目]"+str(eyeMakeup)+" "+"[口]"+str(lipMakeup)+"\n"
+	    #フォントサイズを動的に変える
+    	ttfontname = "./font/HannariMincho-Regular.otf"
+    	fontsize = int(rect['width']/6)
+    	if fontsize >= 10:
+        	fontsize = 10       
+    	fnt = ImageFont.truetype(ttfontname, fontsize)
 	    draw = ImageDraw.Draw(img)
 	    draw.text((rect['left'],rect['top']-120),text, font=fnt, fill='white')
 	    draw.rectangle([(rect['left'],rect['top']),(rect['left']+rect['width'],rect['top']+rect['height'])],fill=None,outline='green',width=5)
