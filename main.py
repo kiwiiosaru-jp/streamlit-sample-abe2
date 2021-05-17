fdd24013ae987de81a0159126afd24786ddb56ed
kiwiiosaru-jp <84110083+kiwiiosaru-jp@users.noreply.github.com>
2021-05-17 14:44:40 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -122,3 +122,17 @@ if upload_file is not None:
 	#st.write(fontsize)
 	#st.write(option)
 	#st.write(df['fst_column'][1])
+	
+from google.cloud import vision
+with open('./nuts.jpg','rb') as image_file:
+	content = image_file.read()
+image = vision.Image(content = content)
+#ImageAnnotatorClientのインスタンスを生成
+annotator_client = vision.ImageAnnotatorClient()
+response_data = annotator_client.label_detection(image=image)
+labels = response_data.label_annotations
+st.write("----RESULT----")
+for label in labels:
+	#print(label.description, ':', round(label.score * 100, 2), '%')
+	st.write(label.description, ':', round(label.score * 100, 2), '%')
+st.write("----RESULT----")
